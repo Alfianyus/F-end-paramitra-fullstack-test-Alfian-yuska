34b4b57508ee8815257f8d0920df2e6618ffb6ea
alfian.yuska <alfian.yuska@phiraka.com>
2023-07-13 08:02:01 +0700

    first commit

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 function Header() {
     return (
         <React.Fragment>
-            <nav className="navbar navbar-expand-lg bg-primary" >
+            <nav className="navbar navbar-expand-lg navbar-light bg-primary" >
                 <div className="container">
                     <NavLink to="/" className="navbar-brand">App</NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
